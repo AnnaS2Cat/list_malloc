95192b3a5aab92441417da14aca17547bc96d38d
AnnaS2Cat <annagraduacaouerj@gmail.com>
2025-12-07 09:55:37 -0300

    Fix variable case in concatena function

--- questão_7.c
+++ questão_7.c
@@ -1,9 +1,9 @@
 struct Caixa* concatena(struct Caixa* L1, struct Caixa* L2) {
 
-    if (l1 == NULL)  //se a primeira lista estiver vazia
+    if (L1 == NULL)  //se a primeira lista estiver vazia
         return L2;
 
-    if (l2 == NULL) //se a segunda lista estiver vazia
+    if (L2 == NULL) //se a segunda lista estiver vazia
         return L1;
 
     struct Caixa* aux = L1;   // percorre L1 até o último nó
